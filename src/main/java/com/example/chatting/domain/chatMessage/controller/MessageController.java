231aff13e1586a8ba1953952788af1617d5ba674
GaBaljaintheroom <junsu1222@naver.com>
2023-07-03 19:21:20 +0900

    feat: 채팅 메시지 controller 작성

--- src/main/java/com/example/chatting/domain/chatMessage/controller/MessageController.java
+++ src/main/java/com/example/chatting/domain/chatMessage/controller/MessageController.java
@@ -16,10 +16,27 @@ import org.springframework.stereotype.Controller;
 public class MessageController {
 
     private final MessageService messageService;
+
+    // 채팅방 입장
     @MessageMapping("/chat/enter/{roomId}")
     @SendTo("/sub/chat/room/{roomId}")
     public MessageInfo enterUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateRequest message){
         message.setMessage(message.getSender() + "님이 채팅방에 입장하였습니다.");
+        return messageService.saveMessage(message);
+    }
+
+    // 채팅방 대화
+    @MessageMapping("/chat/talk/{roomId}")
+    @SendTo("/sub/chat/room/{roomId}")
+    public MessageInfo talkUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateRequest message){
+        return messageService.saveMessage(message);
+    }
+
+    // 채팅방 퇴장
+    @MessageMapping("/chat/exit/{roomId}")
+    @SendTo("/sub/chat/room/{roomId}")
+    public MessageInfo exitUser(@DestinationVariable("roomId") Long roomId, @Payload MessageCreateRequest message){
+        message.setMessage(message.getSender() + "님이 채팅방에 퇴장하였습니다.");
         return messageService.saveMessage(message);
     }
 }
