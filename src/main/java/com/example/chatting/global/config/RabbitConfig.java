af42943d4c2d8606d85ee08ba8ab507f39889cb3
GaBaljaintheroom <junsu1222@naver.com>
2023-07-07 12:42:06 +0900

    feat: RabbitConfig 설정 추가

--- src/main/java/com/example/chatting/global/config/RabbitConfig.java
+++ src/main/java/com/example/chatting/global/config/RabbitConfig.java
@@ -5,6 +5,10 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,5 +40,32 @@ public class RabbitConfig {
                 .bind(queue)
                 .to(exchange)
                 .with(ROUTING_KEY);
+    }
+
+
+    // RabbitMQ와의 메시지 통신을 담당하는 클래스
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    // RabbitMQ와의 연결을 관리하는 클래스
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(61613);
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        return factory;
+    }
+
+    // 메시지를 JSON형식으로 직렬화하고 역직렬화하는데 사용되는 변환기
+    // RabbitMQ 메시지를 JSON형식으로 보내고 받을 수 있음
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
